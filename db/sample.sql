ebe39d3b8086c56fb4374c1e679cf3f951166a51
Junha Park <jpark3@scu.edu>
2020-04-19 23:55:26 -0700

    Fixed indents

--- db/sample.sql
+++ db/sample.sql
@@ -4,21 +4,21 @@ USE SampleInventory;
 
 DROP TABLE IF EXISTS Inventory;
 CREATE TABLE IF NOT EXISTS Inventory (
-	  item_uid INT NOT NULL AUTO_INCREMENT
-    , item_name VARCHAR(64) UNIQUE
-    , quantity INT
-    , PRIMARY KEY (item_uid)
+  item_uid INT NOT NULL AUTO_INCREMENT
+  , item_name VARCHAR(64) UNIQUE
+  , quantity INT
+  , PRIMARY KEY (item_uid)
 )
 ;
 
 INSERT INTO Inventory
 (
-	  item_name
-    , quantity
+  item_name
+  , quantity
 )
 VALUES
 (
-	  'Apple'
-    , 3
+  'Apple'
+  , 3
 )
 ;
